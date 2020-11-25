5e425a63e3107377b3d3adb3c0e132d8eda1e49f
liujiaqing <liujiaqing@mananacare.com>
2020-11-25 16:56:01 +0800

    增加scss

--- src/page/layout/index.tsx
+++ src/page/layout/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+const styles = require('./style.module.scss');
+
 const Layout: React.FunctionComponent = () => {
-	return <div></div>;
+	return <div className={styles['test']}>123412</div>;
 };
 
 export default Layout;
