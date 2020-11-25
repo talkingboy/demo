4e95ff646a5caf18845dda19281a89bdeedd8138
爱因斯坦赤道 <2664707780@qq.com>
2020-11-25 23:05:08 +0800

    增加一些头部样式

--- src/components/head/index.tsx
+++ src/components/head/index.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import logo from "../../assets/logo.jpg";
+const styles = require("./style.module.scss");
 
 const Head: React.FunctionComponent = () => {
   const [menus] = useState(["首页", "旅游攻略", "酒店", "国内机票"]);
   return (
     <header>
-      <div>
-        <div className="logo">
-          <img src={logo} alt="logo"/>
+      <div className={styles["left"]}>
+        <div className={styles["logo"]}>
+          <img alt="logo" src={logo} />
         </div>
         <nav>
           <ul>
