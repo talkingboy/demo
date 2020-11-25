7218b50a2bf8380c948276a81c5a7f4c469f3512
爱因斯坦赤道 <2664707780@qq.com>
2020-11-25 21:58:43 +0800

    修改报错问题，增加node-sass

--- src/page/layout/index.tsx
+++ src/page/layout/index.tsx
@@ -1,37 +1,42 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { GlobalState } from '../../redux/state';
+import React from "react";
+import { connect } from "react-redux";
+import { GlobalState } from "../../redux/state";
+import { Dispatch } from "redux";
+import { setTestValue } from "../../redux/actions";
 
-const styles = require('./style.module.scss');
+const styles = require("./style.module.scss");
 
 type TProps = {
-	setTestValue: (value: number) => void;
-	value: number;
+  setTestValue: (value: number) => void;
+  value: number;
 };
 
-const Layout: React.FunctionComponent<TProps> = ({ setTestValue, value }: TProps) => {
-	return (
-		<>
-			<div className={styles['test']}>{value}</div>
-			<div onClick={() => setTestValue(444)}>1234</div>
-		</>
-	);
+const Layout: React.FunctionComponent<TProps> = ({
+  setTestValue,
+  value,
+}: TProps) => {
+  return (
+    <>
+      <div className={styles["test"]}>{value}</div>
+      <div onClick={() => setTestValue(444)}>1234</div>
+    </>
+  );
 };
 
 const stateToProps = (state: GlobalState) => {
-	return {
-		value: state.value,
-	};
+  return {
+    value: state.value,
+  };
 };
 
 //3.这里把action里的方法绑定到props上，可以通过Props使用，一般用于修改store里的数据
-const dispatchTOProps = (dispatch: (arg0: { type: string; value?: any }) => void) => {
-	return {
-		setTestValue(value: number) {
-			//setTestValue方法会通过dispatch触发reducer.js里的修改方法
-			dispatch({ type: 'setTestValue', value });
-		},
-	};
+const dispatchTOProps = (dispatch: Dispatch) => {
+  return {
+    setTestValue(value: number) {
+      //setTestValue方法会通过dispatch触发reducer.js里的修改方法
+      dispatch(setTestValue(value));
+    },
+  };
 };
 
 export default connect(stateToProps, dispatchTOProps)(Layout);
