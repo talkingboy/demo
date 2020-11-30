19bf847611ed181001b05a7896b92773d7c50d0f
liujiaqing <liujiaqing@mananacare.com>
2020-11-30 14:33:17 +0800

    解决一些报错问题

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ const App: React.FunctionComponent = () => {
 			{/* switch只显示第一次匹配到的页面，如不使用switch则会显示所有匹配到的页面 */}
 			<Switch>
 				{/* component和render：使用component引用组件会在组件没有变化的时候不重新渲染组件，而render则会重新渲染 */}
-				<Route path='/' exact component={() => <Layout></Layout>}></Route>
-				<Route path='/page404' exact component={() => <Page404></Page404>}></Route>
+				<Route path='/' exact render={() => <Layout></Layout>}></Route>
+				<Route path='/page404' exact render={() => <Page404></Page404>}></Route>
 				<Redirect from='/*' to='/page404'></Redirect>
 			</Switch>
 		</BrowserRouter>
